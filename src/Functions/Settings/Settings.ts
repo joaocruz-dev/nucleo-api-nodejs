97a564c9c512f5b5642f39fe2fae3d1c8a68df52
João Cruz <jcruzaraujoneto@outlook.com>
2021-07-22 18:50:44 -0400

    updated Settings functions

--- src/Functions/Settings/Settings.ts
+++ src/Functions/Settings/Settings.ts
@@ -1,7 +1,10 @@
 import * as fs from 'fs'
+import * as path from 'path'
 
-import { randomString } from 'src/Utils'
+import { randomString } from '../../Utils'
 import { OptionsServer, OptionsMetadata } from '../../types/options'
+
+const metadata = path.join(__dirname, './metadata.json')
 
 export default class Settings {
   static get version (): string { return Settings.getMetadata().version }
@@ -88,40 +91,41 @@ export default class Settings {
   }
 
   static getMetadata (): OptionsMetadata {
-    const data = fs.readFileSync('./metadata.json', 'utf8')
+    if (!fs.existsSync(metadata)) return defaultOptions
+    const data = fs.readFileSync(metadata, 'utf8')
     return JSON.parse(data)
   }
 
   static setMetadata (options: OptionsServer): void {
     const _options = Settings.getMetadata()
-  
+
     _options.version = options.version
     _options.sandbox = options.sandbox
-  
+
     _options.server.port.dev = options.server.port.dev
     _options.server.port.prod = options.server.port.prod
     _options.server.port.sandbox = options.server.port.sandbox
-  
+
     _options.server.multiprocessing.dev = options.server.multiprocessing.dev
     _options.server.multiprocessing.prod = options.server.multiprocessing.prod
     _options.server.multiprocessing.sandbox = options.server.multiprocessing.sandbox
-  
+
     _options.endpoint.api.dev = options.endpoint.api.dev
     _options.endpoint.api.prod = options.endpoint.api.prod
     _options.endpoint.api.sandbox = options.endpoint.api.sandbox
-  
+
     _options.endpoint.view.dev = options.endpoint.view.dev
     _options.endpoint.view.prod = options.endpoint.view.prod
     _options.endpoint.view.sandbox = options.endpoint.view.sandbox
-  
+
     _options.mongodb.name = options.mongodb.name
-  
+
     _options.mongodb.conection.dev = options.mongodb.conection.dev
     _options.mongodb.conection.prod = options.mongodb.conection.prod
     _options.mongodb.conection.sandbox = options.mongodb.conection.sandbox
-  
+
     const data = JSON.stringify(_options)
-    fs.writeFileSync('./metadata.json', data, 'utf8')
+    fs.writeFileSync(metadata, data, 'utf8')
   }
 
   static setSecret (): void {
@@ -129,9 +133,50 @@ export default class Settings {
 
     let secret = _options.token.deafult
     if (!Settings.isDevelopment) secret = randomString(1024)
-  
+
     _options.token.secret = secret
     const data = JSON.stringify(_options)
-    fs.writeFileSync('./metadata.json', data, 'utf8')
+    fs.writeFileSync(metadata, data, 'utf8')
+  }
+}
+
+const defaultOptions = {
+  version: '0.0.1',
+  sandbox: false,
+  server: {
+    port: {
+      dev: 3000,
+      prod: 3000,
+      sandbox: 3000
+    },
+    multiprocessing: {
+      dev: false,
+      prod: true,
+      sandbox: true
+    }
+  },
+  endpoint: {
+    api: {
+      dev: '',
+      prod: '',
+      sandbox: ''
+    },
+    view: {
+      dev: '',
+      prod: '',
+      sandbox: ''
+    }
+  },
+  mongodb: {
+    name: '',
+    conection: {
+      dev: '',
+      prod: '',
+      sandbox: ''
+    }
+  },
+  token: {
+    secret: '',
+    deafult: 'bwOmXXX36UKHaRflHgqPxXonklfMFdbdQDN7tlPd42uxJmvuHh'
   }
 }
